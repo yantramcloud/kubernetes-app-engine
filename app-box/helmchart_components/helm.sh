efa6f2ca2b973cbb730346495dc1d4a17178e960
Jayant Kaushal <awakenrogue@gmail.com>
2021-03-01 14:45:23 +0530

    moved ansible and helm chart components in a dedicated componet package. We will move terraform also here. but not just now.

--- app-box/helmchart_components/helm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# author: Jayant Kaushal
-# site: http://yantram.cloud
-# purpose: Create a nginx service in kubernetes cluster
-
-
-
-helm show values prometheus-community/kube-prometheus-stack > kube-prometheus-stack.yaml
